f03c5050c418170192bdfd8c98c2eb440f91f3d5
shubhangidongare007-sys <shubhangidongare007@gmail.com>
2026-07-04 07:52:40 -0700

    Add files via upload

---
+++ 85.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+v1 = np.array([2,4,6,8])
+v2 = np.array([1,3,5,7])
+
+print("Addition:", v1+v2)
+print("Subtraction:", v1-v2)
+print("Element-wise Multiplication:", v1*v2)
+print("Dot Product:", np.dot(v1,v2))
