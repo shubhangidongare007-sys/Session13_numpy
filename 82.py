3e9e11402fe827e13c2f594a7ca7a57d1f6dda32
shubhangidongare007-sys <shubhangidongare007@gmail.com>
2026-07-04 07:51:32 -0700

    Add files via upload

---
+++ 82.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.arange(0,21,1)
+b = np.arange(10,51,2)
+c = np.arange(5,101,5)
+
+print("0 to 20:")
+print(a)
+
+print("\nEven Numbers 10 to 50:")
+print(b)
+
+print("\n5 to 100 Step 5:")
+print(c)
