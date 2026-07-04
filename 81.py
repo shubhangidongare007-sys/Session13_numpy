d119bf94d28942d9dd699ba60a13387c60cc6311
shubhangidongare007-sys <shubhangidongare007@gmail.com>
2026-07-04 07:51:11 -0700

    Add files via upload

---
+++ 81.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.zeros(8)
+b = np.ones((4,4))
+c = np.zeros((3,3))
+
+print("1D Array of Zeros:")
+print(a)
+
+print("\n4x4 Array of Ones:")
+print(b)
+
+print("\n3x3 Matrix of Zeros:")
+print(c)
