6f05ff13342a7ea4e94a5bad6b639e25abaa5b8c
shubhangidongare007-sys <shubhangidongare007@gmail.com>
2026-07-04 07:53:21 -0700

    Add files via upload

---
+++ 87.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+A = np.random.randint(1,101,(4,4))
+
+print(A)
+print("Shape:", A.shape)
+print("Dimension:", A.ndim)
+print("Size:", A.size)
+print("Data Type:", A.dtype)
+print("Minimum:", A.min())
+print("Maximum:", A.max())
