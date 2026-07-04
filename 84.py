05abe650555846a1d05b17a4954df2c4b3a51fd6
shubhangidongare007-sys <shubhangidongare007@gmail.com>
2026-07-04 07:52:16 -0700

    Add files via upload

---
+++ 84.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.random.rand(10)
+b = np.random.randn(3,3)
+c = np.random.randint(10,51,(4,5))
+
+print("Random Numbers:")
+print(a)
+
+print("\nRandom Normal Matrix:")
+print(b)
+
+print("\nRandom Integer Matrix:")
+print(c)
